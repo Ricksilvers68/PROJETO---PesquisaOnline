09804129400065db5cc2798b2827309b1c111e5d
Gustavo de Alencar Barros Rocha <gustavo.rocha3@fatec.sp.gov.br>
2021-11-29 20:07:35 -0200

    ajustando mais algumas rotas

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,4 @@ router.get('/', function(req, res, next) {
     res.render('index', { title: 'Smart List' });
 });
 
-router.get('/user', function(req, res, next) {
-    res.render('user', { title: 'Smart List - Página do Usuário' });
-});
-
 module.exports = router;
--- routes/market.js
+++ routes/market.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET market page. */
 router.get('/', function(req, res, next) {
     res.render('market', { title: 'Smart List - Página do Mercado' });
 });
