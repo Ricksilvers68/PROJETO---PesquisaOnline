ce53a57703f4307dd02c150e2fce1638b9cfe44b
Ricksilvers68 <ricardosilveira68@hotmail.com>
2021-12-04 13:25:30 -0300

    mensagem retorno para erro no formulário

--- controllers/cadSuperController.js
+++ controllers/cadSuperController.js
@@ -16,16 +16,15 @@ const cadSuperController = {
 
     formCad:(req, res)=>{
         let errors = validationResult(req)
-        if(errors.isEmpty()){   //para verificar se há erros no formulário O campo por acaso está vazio? 
+        if(errors.isEmpty()){  
             let {userName, userEmail, password} = req.body
-            //seguimos adiante se não houver erros
             let passwordC = bcrypt.hashSync(password,10)                                             
             let usuario = JSON.stringify({userName, userEmail, password:passwordC})
             fs.appendFileSync(usuarioJson, usuario)
             res.redirect("dados_super")
 
         }else{
-            return res.render("cad_super",{errors:errors.mapped(), old: req.body}) //se houver erros, voltamos ao formulário com as mensagens de erros. Com o método mapped teremos um objeto literal com os erros. Através do old:req.body, enviaremos o restante dos dados preenchidos pelo usuário "os que não estão com erros"
+            return res.send({errors:errors.array()})
         }
         
     }
