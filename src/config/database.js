e8f21f0a66037c2b385a1027898d37fae89427f1
Ricksilvers68 <ricardosilveira68@hotmail.com>
2021-12-30 20:24:08 -0300

    iniciando banco de dados com mysql,sequelize

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+    host:"localhost",
+    dialect:"mysql",
+    username:"testUser",
+    password:"rick",
+    database:"cadastro-usuario",
+define:{
+        timestamps:true,
+        underscored: true
+    }
+
+    }
