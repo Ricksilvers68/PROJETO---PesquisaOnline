4007b466a49075428e887edf3a14fa85df2ab064
Ricksilvers68 <ricardosilveira68@hotmail.com>
2022-01-08 18:41:01 -0300

    importando a base de dados na index.js

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ var middleware = require("./middlewares/logRoutes")
 
 
 var app = express();
+require("./src/database")//para informar as configurações da database/index.js
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -40,12 +41,12 @@ app.use('/', indexRouter);
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
