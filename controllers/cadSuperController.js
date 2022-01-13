a13be6c901bf873a8426affdbc6d1854c945de6b
Ricksilvers68 <ricardosilveira68@hotmail.com>
2022-01-12 21:03:40 -0300

    Recuperando dados do form, e inserindo no BD

--- controllers/cadSuperController.js
+++ controllers/cadSuperController.js
@@ -5,6 +5,13 @@ const bcrypt = require("bcrypt")
 const fs = require("fs")
 const path = require("path")
 
+
+
+const User = require("../src/models/User")
+const Sequelize = require("sequelize")
+
+
+
 let hash = bcrypt.hashSync("46780545", 10)
 let usuarioJson = path.join("usuarios.json")
 
@@ -13,20 +20,22 @@ const cadSuperController = {
         return res.render("cad_super")
     },
 
-    formCad: (req, res) => {
+    formCad: async (req, res) => {
+        let { name, email, password } = req.body
+        let password_c = bcrypt.hashSync(password, 10)
+        let usuario = { name, email, password: password_c }
+        await User.create({ name, email, password_c })
         let errors = validationResult(req)
         if (errors.isEmpty()) {
-            let { name, email, password } = req.body
-            let passwordC = bcrypt.hashSync(password, 10)
-            let usuario = JSON.stringify({ name, email, password: passwordC })
-            fs.appendFileSync(usuarioJson, usuario)
-            res.redirect("dados_super")
+
+            return res.redirect("dados_super")
 
         } else {
             return res.send({ errors: errors.array() })
         }
 
-    }
+    },
+
 }
 
 module.exports = cadSuperController;
