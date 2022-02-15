69122b815d195bf0ee9693689ba685ced6aa5a65
Ricksilvers68 <ricardosilveira68@hotmail.com>
2022-02-15 08:33:22 -0300

    testando try e cath

--- controllers/cadSuperController.js
+++ controllers/cadSuperController.js
@@ -21,19 +21,12 @@ const cadSuperController = {
     },
 
     formCad: async(req, res) => {
-        try {
-            let { name, email, password, flag_usuario } = req.body
-            let password_c = bcrypt.hashSync(password, 10)
-            let usuario = { name, email, password: password_c, flag_usuario }
-            await User.create({ name, email, password_c, flag_usuario }).value
-        } catch (error) {
-            console.log(message.error)
-
-        }
-
-
-        //let usuarios = JSON.stringify({ name, email, password_c, tipo })
-        //fs.appendFileSync(usuarioJson, usuarios)
+        let { name, email, password, flag_usuario } = req.body
+        let password_c = bcrypt.hashSync(password, 10)
+        let usuario = { name, email, password: password_c, flag_usuario }
+        await User.create({ name, email, password_c, flag_usuario }).value
+            //let usuarios = JSON.stringify({ name, email, password_c, tipo })
+            //fs.appendFileSync(usuarioJson, usuarios)
         let errors = validationResult(req)
         if (errors.isEmpty()) {
 
