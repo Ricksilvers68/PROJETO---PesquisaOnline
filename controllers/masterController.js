6f7aeaad04c8da880c1482a60fdf2fcbc4e19cf8
Ricksilvers68 <ricardosilveira68@hotmail.com>
2022-02-26 22:10:45 -0300

    criando rota POST para formulário usuarioMaster

--- controllers/masterController.js
+++ controllers/masterController.js
@@ -6,10 +6,23 @@ const fs = require("fs")
 const path = require("path")
 const Supermercado = require("../src/models/Supermercado")
 
+const userMasterJson = path.join("userMaster.json")
+
 const masterController = {
     master: (req, res) => {
         return res.render("formSuper")
+    },
+
+
+    resForm: (req, res) => {
+        let { nome, email, password } = req.body
+        password_c = bcrypt.hashSync(password, 10)
+        let user = { nome, email, password: password_c }
+        let userMaster = JSON.stringify({ nome, email, password_c })
+        fs.appendFileSync(userMasterJson, userMaster)
+        return res.render("dados_super")
     }
 }
+
 
 module.exports = masterController
