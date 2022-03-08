06a864098160977f2a28111e944da9362547c17d
Gustavo de Alencar Barros Rocha <gustavo.rocha3@fatec.sp.gov.br>
2022-03-08 18:36:13 -0300

    att

--- src/models/User.js
+++ src/models/User.js
@@ -1,18 +1,3 @@
-const { Model, DataTypes } = require("sequelize")
-
-class User extends Model {
-    static init(sequelize) {
-        super.init({
-            name: DataTypes.STRING,
-            email: DataTypes.STRING,
-            password_c: DataTypes.STRING,
-            flag_usuario: DataTypes.STRING
-        }, { sequelize })
-    }
-}
-
-module.exports = User
-
 'use strict';
 const {
   Model
