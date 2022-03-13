16024ff5ae189f4af38c7fd302298590a35501aa
Gustavo de Alencar Barros Rocha <gustavo.rocha3@fatec.sp.gov.br>
2022-03-13 16:07:39 -0300

    ajustando algumas models e criando page 404

--- src/models/UserMasterSup.js
+++ src/models/UserMasterSup.js
@@ -1,26 +1,24 @@
 'use strict';
-const  { Model } = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
+const  { Model, DataTypes } = require('sequelize');
+
   class UserMasterSup extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+    static init(sequelize) {
+      super.init({
+        nome: DataTypes.STRING,
+        user: DataTypes.STRING,
+        password: DataTypes.STRING,
+        flag_usuario: DataTypes.STRING
+      }, {
+        sequelize,
+        modelName: 'UserMasterSup',
+      });
+      return UserMasterSup;
+  }
     static associate(models) {
           UserMasterSup.hasMany(models.Supermercado, {
             as: 'supermercado'
           })
     }
   }
-  UserMasterSup.init({
-    nome: DataTypes.STRING,
-    user: DataTypes.STRING,
-    password: DataTypes.STRING,
-    flag_usuario: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'UserMasterSup',
-  });
-  return UserMasterSup;
-};
+ 
+  module.exports = UserMasterSup;
